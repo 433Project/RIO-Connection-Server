5ce99c10d8c9e4b7e8b7ca16b0e0f45ada50a617
FTT\mikm <etherealtsunami@gmail.com>
2016-10-24 18:18:34 +0900

    AcceptEx Implementation and Experimental/Temporary Connection Management Implementation

--- RIOConnectionServer/RIOManager.cpp
+++ RIOConnectionServer/RIOManager.cpp
@@ -68,96 +68,95 @@ HANDLE RIOManager::CreateIOCP() {
 }
 
 ///This function creates a new RIO Completion Queue with IOCP Queue and Completion Key specified (For Multi-CQ systems with multi-IOCP)
-RIO_CQ RIOManager::CreateCQ(HANDLE hIOCP, COMPLETION_KEY completionKey) {
+CQ_Handler RIOManager::CreateCQ(HANDLE hIOCP, COMPLETION_KEY completionKey) {
+	CQ_Handler cqHandler;
 	OVERLAPPED overlapped;
 	RIO_NOTIFICATION_COMPLETION rioNotificationCompletion;
-	RIO_CQ rioCompletionQueue;
-	CRITICAL_SECTION criticalSection;
 
 	rioNotificationCompletion.Type = RIO_IOCP_COMPLETION;
 	rioNotificationCompletion.Iocp.IocpHandle = hIOCP;
 	rioNotificationCompletion.Iocp.CompletionKey = (void*)completionKey;
 	rioNotificationCompletion.Iocp.Overlapped = &overlapped;
 
-	rioCompletionQueue = rioFunctions.RIOCreateCompletionQueue(
+	cqHandler.rio_CQ = rioFunctions.RIOCreateCompletionQueue(
 		1000,	//MAX_PENDING_RECEIVES + MAX_PENDING_SENDS
 		&rioNotificationCompletion);
-	if (rioCompletionQueue == RIO_INVALID_CQ) {
-		return RIO_INVALID_CQ;
+	if (cqHandler.rio_CQ == RIO_INVALID_CQ) {
+		return cqHandler;
 		//ReportError("RIOCreateCompletionQueue", true);
 	}
 
-	InitializeCriticalSectionAndSpinCount(&criticalSection, 4000); //Add Spin Count Parameter here
+	InitializeCriticalSectionAndSpinCount(&cqHandler.criticalSection, 4000); //Add Spin Count Parameter here
 
 	//*** Need to Store this RIO_CQ handle into the RIO Manager Instance ***
 
-	return rioCompletionQueue;
+	return cqHandler;
 }
 
 ///This function creates a new RIO Completion Queue with default IOCP Queue but custom Completion Key (For creating Multi-CQ system with one IOCP)
-RIO_CQ RIOManager::CreateCQ(COMPLETION_KEY completionKey) {
+CQ_Handler RIOManager::CreateCQ(COMPLETION_KEY completionKey) {
 	return CreateCQ(GetMainIOCP(), completionKey);
 }
 
 ///This function creates a new RIO Completion Queue with IOCP Queue specified (For creating main-CQ in Multi-IOCP system)
-RIO_CQ RIOManager::CreateCQ(HANDLE hIOCP) {
+CQ_Handler RIOManager::CreateCQ(HANDLE hIOCP) {
 	return CreateCQ(hIOCP, CK_RIO);
 }
 
 ///This function creates a new RIO Completion Queue with default values (For creating main-CQ for main-IOCP queue)
-RIO_CQ RIOManager::CreateCQ() {
+CQ_Handler RIOManager::CreateCQ() {
 	return CreateCQ(GetMainIOCP());
 }
 
 ///This function creates a new RIO Socket of various types
-int RIOManager::CreateRIOSocket(SocketType socketType, int port, RIO_CQ receiveCQ, RIO_CQ sendCQ, HANDLE hIOCP) {
-	SOCKET newSocket;
-		sockaddr_in socketAddress;
+int RIOManager::CreateRIOSocket(SocketType socketType, DWORD serviceType, SOCKET newSocket, int port, RIO_CQ receiveCQ, RIO_CQ sendCQ, HANDLE hIOCP) {
+	sockaddr_in socketAddress;
 	socketAddress.sin_family = AF_INET;
 	socketAddress.sin_port = htons(port);
 	socketAddress.sin_addr.s_addr = INADDR_ANY;
 	IPPROTO ipProto;
 	DWORD controlCode;
-	bool isListener;
+	bool isListener, requiresBind;
 	RIO_EXTENSION_FUNCTION_TABLE rioFunctions;
 	LPFN_ACCEPTEX acceptExFunction;
 	RIO_RQ rio_RQ;
 
 	switch (socketType) {
+		//Non-accepted Socket Cases
 	case UDPSocket:
 		ipProto = IPPROTO_UDP;
 		controlCode = SIO_GET_MULTIPLE_EXTENSION_FUNCTION_POINTER;
 		isListener = false;
+		requiresBind = true;
+		newSocket = WSASocket(AF_INET, SOCK_DGRAM, ipProto, NULL, 0, WSA_FLAG_REGISTERED_IO);
 		break;
 	case TCPListener:
 		ipProto = IPPROTO_TCP;
 		controlCode = SIO_GET_EXTENSION_FUNCTION_POINTER;
 		isListener = true;
+		requiresBind = true;
+		newSocket = WSASocket(AF_INET, SOCK_DGRAM, ipProto, NULL, 0, WSA_FLAG_REGISTERED_IO);
 		break;
-	case TCPClient:
-		ipProto = IPPROTO_TCP;
+		//Accepted Socket Cases
+	case TCPConnection:
 		controlCode = SIO_GET_MULTIPLE_EXTENSION_FUNCTION_POINTER;
 		isListener = false;
-		break;
-	case TCPServer:
-		ipProto = IPPROTO_TCP;
-		controlCode = SIO_GET_MULTIPLE_EXTENSION_FUNCTION_POINTER;
-		isListener = false;
+		requiresBind = false;
 		break;
 	default:
 		return -1; //Incorrect socket Type
 	}
-	
-	newSocket = WSASocket(AF_INET, SOCK_DGRAM, ipProto, NULL, 0, WSA_FLAG_REGISTERED_IO);
 
 	if (newSocket == INVALID_SOCKET) {
 		return -2;
 		//ReportError("WSASocket - TCP Server", true);
 	}
 
-	if (SOCKET_ERROR == ::bind(newSocket, reinterpret_cast<struct sockaddr *>(&socketAddress), sizeof(socketAddress))) {
-		return -3;
-		//ReportError("bind - TCP Server", true);
+	if (requiresBind) {
+		if (SOCKET_ERROR == ::bind(newSocket, reinterpret_cast<struct sockaddr *>(&socketAddress), sizeof(socketAddress))) {
+			return -3;
+			//ReportError("bind - TCP Server", true);
+		}
 	}
 
 	if (isListener) {
@@ -184,14 +183,19 @@ int RIOManager::CreateRIOSocket(SocketType socketType, int port, RIO_CQ receiveC
 		hIOCP = ::CreateIoCompletionPort(
 			(HANDLE)newSocket,
 			hIOCP,
-			(ULONG_PTR)CK_ACCEPT_SERVER,			//////////
+			(ULONG_PTR)CK_ACCEPT,			//////////
 			4);								//MAX_CONCURRENT_THREADS
 		if (NULL == hIOCP) {
 			return -6;
 			//ReportError("CreateIoComplectionPort - TCP Server", true);
 		}
+
+		//Create a new service to represent this new listening socket
+		if (CreateNewService(serviceType, port, newSocket) < 0) {
+			return -8;
+		}
 	}
-	else {
+	else {		//Non-Listeners
 		if (NULL != WSAIoctl(
 			newSocket,
 			controlCode,
@@ -207,63 +211,190 @@ int RIOManager::CreateRIOSocket(SocketType socketType, int port, RIO_CQ receiveC
 			//ReportError("WSAIoctl - UDP Socket", true);
 		}
 
+		int socketContext = (int)newSocket;
+
 		rio_RQ = rioFunctions.RIOCreateRequestQueue(
-			newSocket, 1000, 1,		//MAX_PENDING_RECEIVES_UDP, MAX_PENDING_SENDS_UDP
+			newSocket, 1000, 1,				//MAX_PENDING_RECEIVES_UDP, MAX_PENDING_SENDS_UDP
 			1000, 1, receiveCQ,
-			sendCQ, NULL);			//Need to define socket context!!!
+			sendCQ, &socketContext);		//Need to define socket context!!!
 		if (rio_RQ == RIO_INVALID_RQ) {
 			return -7;
 			//ReportError("RIOCreateRequestQueue - UDP Socket", true);
+		}
+
+		//Add a socket to a service
+		if (socketType == UDPSocket) {
+			if (CreateNewService(serviceType, port, newSocket) < 0) {
+				return -8;
+			}
+		}
+		else {
+			if (AddEntryToService(serviceType, socketContext, rio_RQ) < 0) {
+				return -9;
+			}
 		}
 	}
 
 	return 0;
 }
 
-int RIOManager::CreateRIOSocket(SocketType socketType, RIO_CQ receiveCQ, RIO_CQ sendCQ) {
-	return CreateRIOSocket(socketType, 0, receiveCQ, sendCQ, GetMainIOCP());
+int RIOManager::CreateRIOSocket(SocketType socketType, DWORD serviceType, RIO_CQ receiveCQ, RIO_CQ sendCQ) {
+	return CreateRIOSocket(socketType, serviceType, NULL, 0, receiveCQ, sendCQ, INVALID_HANDLE_VALUE);
 }
 
-int RIOManager::CreateRIOSocket(SocketType socketType, int port, RIO_CQ receiveCQ, RIO_CQ sendCQ) {
-	return CreateRIOSocket(socketType, port, receiveCQ, sendCQ, GetMainIOCP());
+///This function allows one to customize the receive/send CQ of a particular service.
+int RIOManager::SetServiceCQs(DWORD typeCode, RIO_CQ receiveCQ, RIO_CQ sendCQ) {
+	//Find the service entry
+	ServiceList::iterator iter = serviceList.find(typeCode);
+	if (iter == serviceList.end()) {
+		return -1;		//Service doesn't exist
+	}
+
+	//Get the service entry
+	ConnectionServerService service;
+	service = iter->second;
+
+	service.receiveCQ = receiveCQ;
+	service.sendCQ = sendCQ;
+
+	return 0;
 }
 
-int RIOManager::CreateRIOSocket(SocketType socketType, int port, HANDLE hIOCP) {
-	return CreateRIOSocket(socketType, port, GetMainRIOCQ(), GetMainRIOCQ(), hIOCP);
+///This function processes an AcceptEx completion by creating a new RIOSocket with the appropriate settings.
+int RIOManager::NewConnection(EXTENDED_OVERLAPPED* extendedOverlapped) {
+	RIO_CQ serviceCQs[2];
+
+	//Find the service entry
+	ServiceList::iterator iter = serviceList.find((*extendedOverlapped).identifier);
+	if (iter == serviceList.end()) {
+		return NULL;		//Service doesn't exist
+	}
+
+	//Get the service entry
+	ConnectionServerService service;
+	service = iter->second;
+
+	serviceCQs[0] = service.receiveCQ;
+	serviceCQs[1] = service.sendCQ;
+
+	RIO_CQ* pointer = serviceCQs;
+
+	if (CreateRIOSocket(TCPConnection,
+		(*extendedOverlapped).identifier,
+		(*extendedOverlapped).relevantSocket,
+		serviceCQs[0],
+		serviceCQs[1]) < 0)
+	{
+		return -1;
+	}
+
+	BeginAcceptEx(extendedOverlapped);
+
+	//Get Buffer/Post Receives
+
+	return 0;
 }
 
-int RIOManager::CreateRIOSocket(SocketType socketType, int port) {
-	return CreateRIOSocket(socketType, port, GetMainRIOCQ(), GetMainRIOCQ(), GetMainIOCP());
-}
 
-int RIOManager::CreateRIOSocket(SocketType socketType) {
-	return CreateRIOSocket(socketType, 0, GetMainRIOCQ(), GetMainRIOCQ(), GetMainIOCP());
-}
-
+//////PRIVATE FUNCTIONS
 
 ///This function gets the main IOCP
 
 
 
-///This function creates a new service in the RIO Manager service list
-int RIOManager::CreateNewService(DWORD typeCode, string name, int portNumber) {
-	if (serviceList.find(typeCode) == serviceList.end()) {
-		return -1;
-	}
-	if (name.empty) {   /////////////
-		return -2;
+///This function creates a new service in the RIO Manager service list.
+///Note that the receive and send CQs are set to the default value.
+int RIOManager::CreateNewService(DWORD typeCode, int portNumber, SOCKET listeningSocket) {
+	if (serviceList.find(typeCode) != serviceList.end()) {
+		return -1;		//Service already exists
 	}
 
 	ConnectionServerService service;
-
-	service.serviceName = name;
 	service.port = portNumber;
-	//service.socketList = new unordered_map<int, RIO_RQ>;
+	service.listeningSocket = listeningSocket;
+	RIO_CQ mainRIOCQ = GetMainRIOCQ();
+	service.receiveCQ = mainRIOCQ;
+	service.sendCQ = mainRIOCQ;
+	service.socketList = new SocketList();
 
-	//serviceList.insert(typeCode, service);
+	serviceList.insert(std::pair<DWORD, ConnectionServerService>(typeCode, service));
+
+	return 0;
 
 }
 
+
+
+int RIOManager::AddEntryToService(DWORD typeCode, int socketContext, RIO_RQ rioRQ) {
+	//Find the service entry
+	ServiceList::iterator iter = serviceList.find(typeCode);
+	if (iter == serviceList.end()) {
+		return -1;		//Service doesn't exist
+	}
+
+	//Get the service entry
+	ConnectionServerService service;
+	service = iter->second;
+
+	//Get a pointer to the service list in which we will add to
+	SocketList* socketList;
+	socketList = service.socketList;
+
+	if ((*socketList).find(socketContext) != (*socketList).end()) {
+		return -2;		//Particular socket entry already exists
+	}
+
+	if (rioRQ == NULL || rioRQ == RIO_INVALID_RQ) {
+		return -3;		//Invalid RIO_RQ
+	}
+
+	//Add the socket context/ RQ pair into the service
+	(*socketList).insert(std::pair<int, RIO_RQ>(socketContext, rioRQ));
+
+	return 0;
+}
+
+
+SOCKET RIOManager::GetListeningSocket(DWORD typeCode) {
+	//Find the service entry
+	ServiceList::iterator iter = serviceList.find(typeCode);
+	if (iter == serviceList.end()) {
+		return INVALID_SOCKET;		//Service doesn't exist
+	}
+
+	//Get the service entry
+	ConnectionServerService service;
+	service = iter->second;
+
+	if (service.listeningSocket == NULL) {
+		return INVALID_SOCKET;		//Socket Not Assigned
+	}
+
+	return service.listeningSocket;
+}
+
+
+int RIOManager::BeginAcceptEx(EXTENDED_OVERLAPPED* extendedOverlapped) {
+	//Needed for AcceptEx
+	DWORD bytes;
+	char* buffer = NULL;
+
+	if (!(AcceptEx( 
+		GetListeningSocket((*extendedOverlapped).identifier),
+		(*extendedOverlapped).relevantSocket,
+		buffer,
+		0,							//No read
+		sizeof(sockaddr_in) + 16,
+		sizeof(sockaddr_in) + 16,	//MSDN specifies that dwRemoteAddressLength "Cannot be zero."
+		&bytes,
+		extendedOverlapped
+		))) 
+	{
+		return -1;
+	}
+
+	return 0;
+}
 
 
 RIOManager::~RIOManager()
--- RIOConnectionServer/RIOManager.h
+++ RIOConnectionServer/RIOManager.h
@@ -6,6 +6,7 @@ using namespace std;
 enum SocketType {
 	UDPSocket,
 	TCPListener,
+	TCPConnection,
 	TCPClient,
 	TCPServer
 };
@@ -25,6 +26,7 @@ enum OperationType {
 enum COMPLETION_KEY {
 	CK_QUIT,
 	CK_RIO,
+	CK_ACCEPT,
 	CK_ACCEPT_CLIENT,
 	CK_ACCEPT_SERVER
 };
@@ -42,16 +44,27 @@ struct ReceivedData {
 	int length;
 };
 
+struct EXTENDED_OVERLAPPED : OVERLAPPED {
+	DWORD identifier;
+	SOCKET relevantSocket;
+};
+
 struct CQ_Handler {
 	RIO_CQ rio_CQ;
 	CRITICAL_SECTION criticalSection;
 };
 
+typedef std::unordered_map<int, RIO_RQ> SocketList;
+
 struct ConnectionServerService {
-	string serviceName;
 	int port;
-	unordered_map<int, RIO_RQ> socketList;
+	SOCKET listeningSocket;
+	RIO_CQ receiveCQ;
+	RIO_CQ sendCQ;
+	SocketList* socketList;
 };
+
+typedef std::unordered_map<DWORD, ConnectionServerService> ServiceList;
 
 class RIOManager
 {
@@ -62,7 +75,7 @@ class RIOManager
 	DWORD dwBytes = 0;
 
 	//Data Structures for Managing Servers/Clients
-	unordered_map<DWORD, ConnectionServerService> serviceList;	//Keeps track of all generated services
+	ServiceList serviceList;	//Keeps track of all generated services
 
 public:
 	RIOManager();
@@ -72,31 +85,42 @@ public:
 	HANDLE CreateIOCP();						//Creates a new IOCP for the RIO_M instance
 
 	//Overloaded Series of Functions to Create a New RIO Completion Queue
-	RIO_CQ CreateCQ(HANDLE hIOCP, COMPLETION_KEY completionKey);	//IOCP Queue and Completion Key specified (For Multi-CQ system with Multi-IOCP)
-	RIO_CQ CreateCQ(COMPLETION_KEY completionKey);					//Create CQ with default IOCP Queue but custom Completion Key (For creating Multi-CQ system with one IOCP)
-	RIO_CQ CreateCQ(HANDLE hIOCP);									//Create CQ with IOCP Queue specified (For creating main-CQ in Multi-IOCP system)
-	RIO_CQ CreateCQ();												//Default (For creating main-CQ for main-IOCP queue)
+	CQ_Handler CreateCQ(HANDLE hIOCP, COMPLETION_KEY completionKey);	//IOCP Queue and Completion Key specified (For Multi-CQ system with Multi-IOCP)
+	CQ_Handler CreateCQ(COMPLETION_KEY completionKey);					//Create CQ with default IOCP Queue but custom Completion Key (For creating Multi-CQ system with one IOCP)
+	CQ_Handler CreateCQ(HANDLE hIOCP);									//Create CQ with IOCP Queue specified (For creating main-CQ in Multi-IOCP system)
+	CQ_Handler CreateCQ();												//Default (For creating main-CQ for main-IOCP queue)
 
 	//Overloaded Series of Functions to Create a new RIO Socket of various types
-	int CreateRIOSocket(SocketType socketType, int port, RIO_CQ receiveCQ, RIO_CQ sendCQ, HANDLE hIOCP);	//Any Type of Socket 
-	int CreateRIOSocket(SocketType socketType, RIO_CQ receiveCQ, RIO_CQ sendCQ);							//TCP Client or Server with CQs specified
-	int CreateRIOSocket(SocketType socketType, int port, RIO_CQ receiveCQ, RIO_CQ sendCQ);					//UDP Socket with CQs specified
-	int CreateRIOSocket(SocketType socketType, int port, HANDLE hIOCP);										//TCP Listener with IOCP queue specified
-	int CreateRIOSocket(SocketType socketType, int port);													//UDP Socket OR TCP Listener with default handles
-	int CreateRIOSocket(SocketType socketType);																//Any Type with default values
+	int CreateRIOSocket(SocketType socketType, DWORD serviceType, SOCKET newSocket, int port, RIO_CQ receiveCQ, RIO_CQ sendCQ, HANDLE hIOCP);	//Any Type of Socket 
+	int CreateRIOSocket(SocketType socketType, DWORD serviceType, RIO_CQ receiveCQ, RIO_CQ sendCQ);							//TCP Client or Server with CQs specified
+	int CreateRIOSocket(SocketType socketType, DWORD serviceType, int port, RIO_CQ receiveCQ, RIO_CQ sendCQ);				//UDP Socket with CQs specified
+	int CreateRIOSocket(SocketType socketType, int port, HANDLE hIOCP);														//TCP Listener with IOCP queue specified
+	int CreateRIOSocket(SocketType socketType, int port);																	//UDP Socket OR TCP Listener with default handles
+	int CreateRIOSocket(SocketType socketType, DWORD serviceType);																				//Any Type with default values
+	int CreateRIOSocket(SocketType socketType);																				//Any Type with default values
+
+	int SetServiceCQs(DWORD typeCode, RIO_CQ receiveCQ, RIO_CQ sendCQ);
+
 
 	int GetCompletedResults(vector<ReceivedData*>& results) {
 		//results = new ReceivedData*[];
 		//results[0] = new ReceivedData();
 	}
 	int ProcessInstruction(InstructionType instructionType);
+
+	int NewConnection(EXTENDED_OVERLAPPED* extendedOverlapped);
+
 	void Shutdown();
 private:
-	int CreateNewService(DWORD typeCode, string name, int portNumber);
+	int CreateNewService(DWORD typeCode, int portNumber, SOCKET listeningSocket);
+	unordered_map<int, RIO_RQ>* GetService(DWORD typeCode);
+	int AddEntryToService(DWORD typeCode, int socketContext, RIO_RQ rioRQ);
+	SOCKET GetListeningSocket(DWORD typeCode);
 	int CloseSocket();
 	int RegisterIOCP();
 	int RegisterRIOCQ();
 	int RegisterRIORQ();
+	int BeginAcceptEx(EXTENDED_OVERLAPPED* extendedOverlapped);
 	HANDLE GetMainIOCP();
 	RIO_CQ GetMainRIOCQ();
 };
