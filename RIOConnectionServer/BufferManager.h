394d429e81c2dd52c3c73536bbcba4ff44463b9e
FTT\kimsin <kimsin3003@naver.com>
2016-10-28 15:01:40 +0900

    BufferManager version 1 complete.

---
+++ RIOConnectionServer/BufferManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "definitions.h"
+#include <queue>
+#include <vector>
+
+class BufferManager
+{
+public:
+	BufferManager();
+	~BufferManager();
+
+	void Initialize(RIO_EXTENSION_FUNCTION_TABLE& rioFuntionsTable, DWORD bufferCount, DWORD bufferSize);
+	EXTENDED_RIO_BUF* GetBuffer();
+	void FreeBuffer(EXTENDED_RIO_BUF* buffer);
+	void ShutdownCleanup(RIO_EXTENSION_FUNCTION_TABLE& rioFuntionsTable);
+
+private:
+	char* AllocateBufferSpace(const DWORD bufCount, const DWORD bufSize, DWORD& totalBufferSize, DWORD& totalBufferCount);
+
+private:
+	std::vector<EXTENDED_RIO_BUF*> bufferPool;					//buffer segments pool
+	std::queue<int> freeBufferIndex;							//indexes of free buffer segments
+	std::vector<std::pair<RIO_BUFFERID, char*>> rioBuffers;		//rio buffer pointers. used vector to make it scalable.
+	int maxBufferCount;
+	int bufferSize;
+};
+
--- RIOConnectionServer/definitions.h
+++ RIOConnectionServer/definitions.h
@@ -52,7 +52,7 @@ struct EXTENDED_RIO_BUF : public RIO_BUF
 	OperationType operationType;
 	DestinationType srcType;
 	int socketContext;
-	void* buffer;
+	char* buffer;
 	int messageLength;
 
 };
