537adbc3041cb071cd38ffbffa8a8358e4ae2b9a
FTT\kimsin <kimsin3003@naver.com>
2016-11-23 18:26:41 +0900

    atomic test available.

--- RIOConnectionServer/BufferManager.cpp
+++ RIOConnectionServer/BufferManager.cpp
@@ -95,8 +95,10 @@ void BufferManager::FreeBuffer(ExtendedRioBuf* buffer)
 		bool newVal = true;
 		if (isQueueInUse.compare_exchange_strong(testVal, newVal))
 		{
+			std::cout << "in" << std::endl;
 			freeBufferIndex.push(bufferIndex);
 			isQueueInUse = false;
+			return;
 		}
 	}
 
